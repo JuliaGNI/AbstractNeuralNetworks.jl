577fa3ec770f0fb0984d743109d6c6619d55ae92
Michael Kraus <michael.kraus@ipp.mpg.de>
2023-07-11 17:09:19 +0200

    Minor refactoring of activation functions.

--- src/activation.jl
+++ src/activation.jl
@@ -1,6 +1,9 @@
 
-abstract type AbstractActivationFunction end
+abstract type AbstractActivation end
+abstract type ScalarActivation <: AbstractActivation end
+abstract type VectorActivation <: AbstractActivation end
 
-struct IdentityActivation <: AbstractActivationFunction end
+
+struct IdentityActivation <: ScalarActivation end
 
 (::IdentityActivation)(x) = x
