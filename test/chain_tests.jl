19dc825bf19feb0ab7141790aba699cf6ceaf2d8
Michael Kraus <michael.kraus@ipp.mpg.de>
2023-07-11 17:30:12 +0200

    Update Chain tests.

--- test/chain_tests.jl
+++ test/chain_tests.jl
@@ -15,13 +15,15 @@ p1 = initialparameters(Random.default_rng(), Float64, c; init = OneInitializer()
 p2 = initialparameters(Random.default_rng(), CPU(), Float64, c; init = OneInitializer())
 p3 = initialparameters(Float64, c; init = OneInitializer())
 p4 = initialparameters(CPU(), Float64, c; init = OneInitializer())
+p5 = initialparameters(Float64, c; init = OneInitializer(), rng = Random.default_rng())
+p6 = initialparameters(CPU(), Float64, c; init = OneInitializer(), rng = Random.default_rng())
 
-@test p1 == p2 == p3 == p4
+@test p1 == p2 == p3 == p4 == p5 == p6
 
 
 c = Chain(Dense(2, 2, x -> x))
 
-p = initialparameters(Random.default_rng(), Float64, c; init = OneInitializer())
+p = initialparameters(Float64, c; init = OneInitializer())
 
 @test c(i, p) == 3 .* i
 
@@ -29,7 +31,7 @@ p = initialparameters(Random.default_rng(), Float64, c; init = OneInitializer())
 c = Chain(Dense(2, 2, x -> x),
           Dense(2, 2, x -> x))
 
-p = initialparameters(Random.default_rng(), Float64, c; init = OneInitializer())
+p = initialparameters(Float64, c; init = OneInitializer())
 
 @test c(i, p) == 7 .* i
 
@@ -38,6 +40,6 @@ c = Chain(Linear(2, 2),
           Linear(2, 2),
           Linear(2, 2))
 
-p = initialparameters(Random.default_rng(), Float64, c; init = OneInitializer())
+p = initialparameters(Float64, c; init = OneInitializer())
 
 @test c(i, p) == 15 .* i
