845760b3dd2bf87b68efd0165ff1321ce5213443
Michael Kraus <michael.kraus@ipp.mpg.de>
2023-07-11 16:52:05 +0200

    Move chain test file to correct location.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,6 @@ using SafeTestsets
 @safetestset "Abstract Layer                                                                  " begin include("layers/abstract_layer_tests.jl") end
 @safetestset "Dense Layer                                                                     " begin include("layers/dense_layer_tests.jl") end
 @safetestset "Linear Layer                                                                    " begin include("layers/linear_layer_tests.jl") end
-@safetestset "Chain                                                                           " begin include("layers/chain_tests.jl") end
+@safetestset "Chain                                                                           " begin include("chain_tests.jl") end
 @safetestset "Architecture                                                                    " begin include("architecture_tests.jl") end
 @safetestset "Neural Network                                                                  " begin include("neural_network_tests.jl") end
