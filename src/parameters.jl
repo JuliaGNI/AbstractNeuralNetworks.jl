fba99d20cf94e700c402efb2b722a4d437526a29
Michael Kraus <michael.kraus@ipp.mpg.de>
2025-02-04 15:19:19 +0100

    Fix type instability in NeuralNetworkParameters.

--- src/parameters.jl
+++ src/parameters.jl
@@ -11,20 +11,11 @@ end
 
 NeuralNetworkParameters{Keys}(values) where {Keys} = NeuralNetworkParameters(NamedTuple{Keys}(values))
 
-params(p::NeuralNetworkParameters) = p.params
+params(p::NeuralNetworkParameters) = getfield(p, :params)
 
 
-@inline function Base.hasproperty(::NeuralNetworkParameters{Keys}, s::Symbol) where {Keys}
-    s ∈ Keys || hasfield(NeuralNetworkParameters, s)
-end
-
-@inline function Base.getproperty(p::NeuralNetworkParameters{Keys}, s::Symbol) where {Keys}
-    if s ∈ Keys
-        return getfield(p, :params)[s]
-    else
-        return getfield(p, s)
-    end
-end
+Base.hasproperty(::NeuralNetworkParameters{Keys}, s::Symbol) where {Keys} = s ∈ Keys
+Base.getproperty(p::NeuralNetworkParameters{Keys}, s::Symbol) where {Keys} = params(p)[s]
 
 Base.getindex(p::NeuralNetworkParameters, args...) = getindex(params(p), args...)
 Base.keys(p::NeuralNetworkParameters) = keys(params(p))
